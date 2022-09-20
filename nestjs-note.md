33b0f4038c24051d4235467adc4d2467b76fef99
行云流水justdoit <1391020381@qq.com>
2022-09-20 19:48:17 +0800

    feat: add文档,还是阅读英文的文档

---
+++ nestjs-note.md
@@ -0,0 +1,2 @@
+- [热重载](https://docs.nestjs.cn/9/recipes?id=%e7%83%ad%e9%87%8d%e8%bd%bd)
+- [Swagger](https://docs.nestjs.cn/9/recipes?id=swagger)
--- webpack-hmr.config.js
+++ webpack-hmr.config.js
@@ -1,6 +1,7 @@
 const nodeExternals = require('webpack-node-externals');
 const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
 
+// 此函数获取包含默认 webpack配置的原始对象,并返回一个已修改的对象和一个已应用的HotModuleReplacementPlugin 插件
 module.exports = function (options, webpack) {
   return {
     ...options,
