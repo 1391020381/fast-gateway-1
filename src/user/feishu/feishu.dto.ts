79f3d7d30623ed77271f181ab0bdcfa72522fc1d
行云流水justdoit <1391020381@qq.com>
2022-12-13 16:24:43 +0800

    demo/v5

--- src/user/feishu/feishu.dto.ts
+++ src/user/feishu/feishu.dto.ts
@@ -5,19 +5,20 @@ import { IsNotEmpty, IsEnum } from 'class-validator';
 export class FeishuMessageDto {
   @IsNotEmpty()
   @IsEnum(RECEIVE_TYPE)
-  @ApiProperty({ example: 'email', enum: RECEIVE_TYPE })
-  receive_id_type: RECEIVE_TYPE
+  @ApiProperty({ example: 'text', enum: RECEIVE_TYPE })
+  receive_id_type: RECEIVE_TYPE;
 
   @IsNotEmpty()
-  @ApiProperty({ example: 'cookieboty@qq.com' })
-  receive_id?: string
+  // @ApiProperty({ example: 'cookieboty@qq.com' })
+  @ApiProperty({ example: '1391020381@qq.com' })
+  receive_id?: string;
 
   @IsNotEmpty()
-  @ApiProperty({ example: '{\"text\":\" test content\"}' })
-  content?: string
+  @ApiProperty({ example: '{"text":" test content"}' })
+  content?: string;
 
   @IsNotEmpty()
   @IsEnum(MSG_TYPE)
   @ApiProperty({ example: 'text', enum: MSG_TYPE })
-  msg_type?: MSG_TYPE
+  msg_type?: MSG_TYPE;
 }
