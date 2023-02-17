2548c3125da9886605e3edf35741ccba9abc7124
1391020381 <1391020381@qq.com>
2023-02-17 18:05:29 +0800

    feat: 数据库自定义provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,15 +3,16 @@ import { DatabaseModule } from '@/common/database/database.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserProviders } from './user.providers';
-
+/**
+ *  providers 由Nest注入器实例化的提供者,并且可以至少在整个模块中共享
+ *  controllers 必须创建的一组控制器
+ *  imports 倒入模块的类表,这些模块导出了此模块中所需提供者
+ *  exports 由本模块提供并应在其他模块中可用的提供者的子集。
+ */
 @Module({
-  imports: [
-    DatabaseModule
-  ],
-  controllers: [
-    UserController
-  ],
+  imports: [DatabaseModule],
+  controllers: [UserController],
   providers: [...UserProviders, UserService],
   exports: [UserService],
 })
-export class UserModule { }
+export class UserModule {}
--- src/user/user.providers.ts
+++ src/user/user.providers.ts
@@ -1,9 +1,15 @@
 import { User } from './user.mongo.entity';
-
+/**
+ * 工厂提供者(useFactory)
+ * useFactory语法允许动态创建提供程序。
+ * 实工厂函数的返回实际的provider。
+ * https://docs.nestjs.cn/9/fundamentals?id=%e5%bc%82%e6%ad%a5%e6%8f%90%e4%be%9b%e8%80%85
+ */
 export const UserProviders = [
   {
     provide: 'USER_REPOSITORY',
-    useFactory: async (AppDataSource) => await AppDataSource.getRepository(User),
+    useFactory: async (AppDataSource) =>
+      await AppDataSource.getRepository(User),
     inject: ['MONGODB_DATA_SOURCE'],
   },
 ];
